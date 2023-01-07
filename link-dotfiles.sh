34b38a44cd6711d1e5076f1caef8fae7782b66f2
Nick Petalas <webmasternikos@gmail.com>
2023-01-07 13:01:42 +0000

    fixing link-dotfiles existing file check -f to -e (to catch symlinks)

--- link-dotfiles.sh
+++ link-dotfiles.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 
+[ -e $HOME/.zshrc.old ] && rm $HOME/.zshrc.old
+if [ -e $HOME/.zshrc ]; then
+    echo "Creating backup: $HOME/.zshrc.old"
+    cp $HOME/.zshrc $HOME/.zshrc.old
+    rm $HOME/.zshrc
+fi
 echo "Linking $(pwd)/dot/zshrc -> $HOME/.zshrc"
-[ -f $HOME/.zshrc.old ] && rm $HOME/.zshrc.old
-[ -f $HOME/.zshrc ] && echo "Creating backup: $HOME/.zshrc.old" && mv $HOME/.zshrc $HOME/.zshrc.old
 ln -s "$(pwd)/dot/zshrc" $HOME/.zshrc
 
+[ -e $HOME/.gitconfig.old ] && rm $HOME/.gitconfig.old
+if [ -e $HOME/.gitconfig ]; then
+    echo "Creating backup: $HOME/.gitconfig.old"
+    cp $HOME/.gitconfig $HOME/.gitconfig.old
+    rm $HOME/.gitconfig
+fi
 echo "Linking $(pwd)/dot/gitconfig -> $HOME/.gitconfig"
-[ -f $HOME/.gitconfig.old ] && rm $HOME/.gitconfig.old
-[ -f $HOME/.gitconfig ] && echo "Creating backup: $HOME/.gitconfig.old" && mv $HOME/.gitconfig $HOME/.gitconfig.old
 ln -s "$(pwd)/dot/gitconfig" $HOME/.gitconfig
 
 # make sure $HOME/git/genesis exists before linking
@@ -16,7 +24,11 @@ if [ ! -d "$HOME/git/genesis" ]; then
     mkdir -p $HOME/git/genesis
 fi
 
-echo "Linking $(pwd)/dot/genesis/gitconfig -> $HOME/git/genesis/.gitconfig (you might be prompted to delete existing dotfile)"
-[ -f $HOME/git/genesis/.gitconfig.old ] && rm $HOME/git/genesis/.gitconfig.old
-[ -f $HOME/git/genesis/.gitconfig ] && echo "Creating backup: $HOME/.gitconfig.old" && mv $HOME/git/genesis/.gitconfig $HOME/git/genesis/.gitconfig.old
+[ -e $HOME/git/genesis/.gitconfig.old ] && rm $HOME/git/genesis/.gitconfig.old
+if [ -e $HOME/git/genesis/.gitconfig ]; then
+    echo "Creating backup: $HOME/.gitconfig.old"
+    cp $HOME/git/genesis/.gitconfig $HOME/git/genesis/.gitconfig.old
+    rm $HOME/git/genesis/.gitconfig
+fi
+echo "Linking $(pwd)/dot/genesis/gitconfig -> $HOME/git/genesis/.gitconfig"
 ln -s "$(pwd)/dot/genesis/gitconfig" $HOME/git/genesis/.gitconfig
