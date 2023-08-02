70e2650b4d1e120cf7392deb7f3ad4970a27a6bc
Nick Petalas <webmasternikos@gmail.com>
2023-08-02 21:35:44 +0100

    formatting

--- dot/.config/nvim/lua/petalas/packer.lua
+++ dot/.config/nvim/lua/petalas/packer.lua
@@ -10,15 +10,12 @@ return require('packer').startup(function(use)
     use 'wbthomason/packer.nvim'
 
     use {
-        'nvim-telescope/telescope.nvim',
-        tag = '0.1.0', 
-		-- or                            , branch = '0.1.x',
+        'nvim-telescope/telescope.nvim', branch = '0.1.x',
         requires = {{'nvim-lua/plenary.nvim'}}
     }
 
     use({
-        'rose-pine/neovim',
-        as = 'rose-pine',
+        'rose-pine/neovim', as = 'rose-pine',
         config = function()
             vim.cmd('colorscheme rose-pine')
         end
@@ -33,24 +30,37 @@ return require('packer').startup(function(use)
             ts_update()
         end
     })
+
     use("nvim-treesitter/playground")
-    use("theprimeagen/harpoon")
-    use("mbbill/undotree")
     use("nvim-treesitter/nvim-treesitter-context")
+
+    use("mbbill/undotree")
 
     use {
         'VonHeikemen/lsp-zero.nvim',
         branch = 'v1.x',
-        requires = { -- LSP Support
-        {'neovim/nvim-lspconfig'}, {'williamboman/mason-lspconfig.nvim'}, {'williamboman/mason.nvim'},
-
-        -- Autocompletion
-        {'hrsh7th/cmp-buffer'}, {'hrsh7th/cmp-nvim-lsp'}, {'hrsh7th/cmp-nvim-lua'}, {'hrsh7th/cmp-path'},
-        {'hrsh7th/nvim-cmp'}, {'saadparwaiz1/cmp_luasnip'}, -- Snippets
-        {'L3MON4D3/LuaSnip'}, {'rafamadriz/friendly-snippets'}}
+        requires = {
+            -- LSP Support
+            {'neovim/nvim-lspconfig'},
+            {'williamboman/mason-lspconfig.nvim'},
+            {'williamboman/mason.nvim'},
+  
+            -- Autocompletion
+            {'hrsh7th/cmp-buffer'},
+            {'hrsh7th/cmp-nvim-lsp'},
+            {'hrsh7th/cmp-nvim-lua'},
+            {'hrsh7th/cmp-path'},
+            {'hrsh7th/nvim-cmp'},
+            {'saadparwaiz1/cmp_luasnip'},
+  
+            -- Snippets
+            {'L3MON4D3/LuaSnip'},
+            {'rafamadriz/friendly-snippets'},
+        }
     }
 
     use('ThePrimeagen/vim-be-good')
+    use("theprimeagen/harpoon")
 
     -- Automatically set up your configuration after cloning packer.nvim
     -- Put this at the end after all plugins
