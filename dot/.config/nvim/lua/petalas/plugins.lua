f53d83167d8ef2436edd18c8240d43ef612311d6
Nick Petalas <webmasternikos@gmail.com>
2023-08-04 10:44:35 +0100

    gitsigns

--- dot/.config/nvim/lua/petalas/plugins.lua
+++ dot/.config/nvim/lua/petalas/plugins.lua
@@ -97,6 +97,9 @@ return packer.startup(function(use)
     --  Color scheme: https://github.com/folke/tokyonight.nvim
     use("folke/tokyonight.nvim")
 
+    -- Git
+    use("lewis6991/gitsigns.nvim")
+
     -- Automatically set up your configuration after cloning packer.nvim
     -- Put this at the end after all plugins
     if packer_bootstrap then
