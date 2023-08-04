dff93b589de0e4dfe1bb071e16834d33d587e1a6
Nick Petalas <webmasternikos@gmail.com>
2023-08-04 22:30:59 +0100

    setting up toggleterm

--- dot/.config/nvim/after/plugin/chadtree.lua
+++ dot/.config/nvim/after/plugin/chadtree.lua
@@ -3,4 +3,14 @@ if not installed then
     return
 end
 
+local chadtree_settings = {
+    ['options.close_on_open'] = false, -- stay open when opening a file
+}
+vim.api.nvim_set_var("chadtree_settings", chadtree_settings)
+
+-- keybind to toggle
 vim.keymap.set("n", "<leader>v", "<cmd>CHADopen<cr>")
+
+-- open automatically
+vim.cmd('CHADopen')
+
