64d31f21140c22c47d2a6acc14a007db000f0231
Nick Petalas <webmasternikos@gmail.com>
2023-08-05 19:38:02 +0100

    adding compiled plugins to cleanup script

--- reset-nvim.sh
+++ reset-nvim.sh
@@ -2,5 +2,5 @@
 
 if [[ $OSTYPE == "linux"* || $OSTYPE == "darwin"* ]]; then
     echo "resetting neovim cache, plugins, data"
-    rm -rf ~/.cache/nvim ~/.config/nvim/plugin ~/.local/share/nvim
+    rm -rf ~/.cache/nvim ~/.config/nvim/plugin ~/.local/share/nvim ~/.config/nvim/plugin
 fi
