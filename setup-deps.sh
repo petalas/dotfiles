1e34335cd6cfdb7bb88a02b53d61cfdbc3eccc67
Nick Petalas <webmasternikos@gmail.com>
2023-08-07 17:27:33 +0100

    fixing lazygit installation script

--- setup-deps.sh
+++ setup-deps.sh
@@ -30,14 +30,16 @@ function install_node_deps(){
 }
 
 function install_lazygit(){
-    if [[ ( ! $(which lazygit) == *"lazygit") || ( ! $OSTYPE == "linux"* ) ]]; then
+    if [[ ( $(which lazygit) == *"lazygit") || ( ! $OSTYPE == "linux"* ) ]]; then
         return 1
     fi
-    echo "Installing lazygit..."
+    echo "Installing ${yellow}lazygit${reset}..."
     LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
     curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
     tar xf lazygit.tar.gz lazygit
     sudo install lazygit /usr/local/bin
+    rm -f lazygit.tar.gz lazygit
+    echo "Finished installing ${yellow}lazygit${reset}."
 }
 
 function install_sdkman(){
