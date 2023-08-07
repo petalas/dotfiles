b27e34f71949fb54b208983b175df0baa0aad5af
Nick Petalas <webmasternikos@gmail.com>
2023-08-07 20:38:22 +0100

    adding ssh to deps and lazygit alias

--- easy-install.sh
+++ easy-install.sh
@@ -5,7 +5,5 @@ sudo -u $(whoami) ./setup-fonts.sh
 sudo -u $(whoami) ./setup-zsh.sh
 sudo -u $(whoami) ./link-dotfiles.sh
 
-# launch new alacritty terminal,
-# nerd font should already be set by linked config file
-# run p10k configure if it does not run automatically (it should)
-alacritty &
+echo "Please log out and open an alacritty terminal when you log back in."
+echo "The configuration wizard for p10k should run by itself, if not run `p10k configure`"
