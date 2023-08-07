1baee54fa6a9be0369fed804b168dfbdad56aa9c
Nick Petalas <webmasternikos@gmail.com>
2023-08-07 17:33:27 +0100

    working on font installation script

--- easy-install.sh
+++ easy-install.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 # Just to avoid asking for password multiple times but still run as the current user.
 sudo -u $(whoami) ./setup-deps.sh
+sudo -u $(whoami) ./setup-fonts.sh
+sudo -u $(whoami) ./link-dotfiles.sh
 sudo -u $(whoami) ./setup-zsh.sh
