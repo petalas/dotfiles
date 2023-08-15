2089f6fcaa28b3c2ca0708b7fd3e87554ca6c16b
Nick Petalas <webmasternikos@gmail.com>
2023-08-15 20:28:05 +0100

    adding indent-blankine plugin + config

---
+++ dot/.config/nvim/after/plugin/ibl.lua
@@ -0,0 +1,28 @@
+-- https://github.com/lukas-reineke/indent-blankline.nvim
+
+local installed, ibl = pcall(require, "indent_blankline")
+if not installed then
+	return
+end
+
+ibl.setup({
+	space_char_blankline = " ",
+	char_highlight_list = {
+		"IndentBlanklineIndent1",
+		"IndentBlanklineIndent2",
+		"IndentBlanklineIndent3",
+		"IndentBlanklineIndent4",
+		"IndentBlanklineIndent5",
+		"IndentBlanklineIndent6",
+	},
+	show_current_context = true,
+	show_current_context_start = true,
+})
+
+-- requires vim.opt.termguicolors = true (set in options.lua)
+vim.cmd([[highlight IndentBlanklineIndent1 guifg=#E06C75 gui=nocombine]])
+vim.cmd([[highlight IndentBlanklineIndent2 guifg=#E5C07B gui=nocombine]])
+vim.cmd([[highlight IndentBlanklineIndent3 guifg=#98C379 gui=nocombine]])
+vim.cmd([[highlight IndentBlanklineIndent4 guifg=#56B6C2 gui=nocombine]])
+vim.cmd([[highlight IndentBlanklineIndent5 guifg=#61AFEF gui=nocombine]])
+vim.cmd([[highlight IndentBlanklineIndent6 guifg=#C678DD gui=nocombine]])
--- dot/.config/nvim/lua/petalas/plugins.lua
+++ dot/.config/nvim/lua/petalas/plugins.lua
@@ -114,6 +114,9 @@ return packer.startup(function(use)
 		requires = { "nvim-tree/nvim-web-devicons", opt = true },
 	})
 
+    -- adds indentation guides
+	use("lukas-reineke/indent-blankline.nvim")
+
 	-- Automatically set up your configuration after cloning packer.nvim
 	-- Put this at the end after all plugins
 	if packer_bootstrap then
