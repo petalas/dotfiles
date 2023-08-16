7c225475f41e0c5246af27d0db8a30db6f1bd9aa
Nick Petalas <webmasternikos@gmail.com>
2023-08-17 00:00:44 +0100

    fixed tree-sitter auto_install

--- dot/.config/nvim/lua/custom/configs/treesitter.lua
+++
@@ -1,9 +0,0 @@
-local installed, treesitter = pcall(require, "nvim-treesitter")
-if not installed then
-	vim.notify("treesitter not installed")
-	return
-end
-
-treesitter.setup({
-	auto_install = true,
-})
