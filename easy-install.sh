3b7905855c806729341ec03ae36be7c0f4c015b1
Nick Petalas <webmasternikos@gmail.com>
2024-01-31 10:26:51 +0000

    install script improvements

--- easy-install.sh
+++ easy-install.sh
@@ -6,11 +6,22 @@ yellow=$(tput setaf 3)
 reset=$(tput sgr0)
 
 # Just to avoid asking for password multiple times but still run as the current user.
+echo "${yellow}easy install -> setting up dependencies${reset}\n"
 sudo -u $(whoami) ./setup-deps.sh
+
+echo "${yellow}easy install -> setting up fonts${reset}\n"
 sudo -u $(whoami) ./setup-fonts.sh
+
+echo "${yellow}easy install -> linking dotfiles${reset}\n"
 sudo -u $(whoami) ./link-dotfiles.sh
+
+echo "${yellow}easy install -> setting up zsh${reset}\n"
 sudo -u $(whoami) ./setup-zsh.sh
+
+echo "${yellow}easy install -> configuring zsh${reset}\n"
 sudo -u $(whoami) ./configure-zsh.sh
+
+source ~/.zshrc
 
 printf "\n\nIf this is a fresh installation:\n"
 echo "Please ${yellow}log out${reset} (for chsh to take effect) and open an ${green}alacritty terminal${reset} when you log back in."
